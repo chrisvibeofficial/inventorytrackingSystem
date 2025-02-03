f93f2ece97bdb0329d62ee788adf16ddc18a8975
chrisvibeofficial <jueffizzy@gmail.com>
2025-02-03 16:14:16 +0100

    another update

--- controller/orderItemController.js
+++ controller/orderItemController.js
@@ -1,6 +1,7 @@
 const orderItemModel = require('../models/order_item');
 const productModel = require('../models/product');
-const orderModel = require('../models/order')
+const orderModel = require('../models/order');
+const { where } = require('sequelize');
 
 exports.createOrderItem = async (req, res) => {
   try {
@@ -8,7 +9,7 @@ exports.createOrderItem = async (req, res) => {
     const { orderId, quantity } = req.body
     const checkProduct = await productModel.findOne({ where: { id: productId } });
     console.log(checkProduct);
-    
+
     if (!checkProduct) {
       return res.status(404).json('Product is unavailable')
     }
@@ -28,6 +29,13 @@ exports.createOrderItem = async (req, res) => {
       message: 'Items listed successfully',
       data: newOrderItem
     })
+
+    if (newOrderItem) {
+      const checkStock = checkProduct.dataValues.stock
+      const newStock = checkProduct.dataValues.stock - checkStock
+      await productModel.update(newStock, { where: { stock: checkStock } })
+    }
+
   } catch (error) {
     res.status(500).json({
       message: 'Internal server error',
